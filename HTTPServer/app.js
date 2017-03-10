80718c1460483b98ff40daa820d9783c777755fc
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-03-10 14:41:04 -0500

    more to http server

--- HTTPServer/app.js
+++ HTTPServer/app.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res){
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hello World! \n');
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var htmlResponse = fs.readFileSync(__dirname + '/index.html');
+    res.end(htmlResponse);
 
 }).listen(12345,'127.0.0.1');
