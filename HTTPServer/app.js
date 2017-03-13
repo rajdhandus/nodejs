fcd3a0331a76a8fdbae7180558c3f7f819af7acf
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-03-13 14:43:01 -0400

    AsyncServer using stream

--- HTTPServer/app.js
+++ HTTPServer/app.js
@@ -3,7 +3,9 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
     res.writeHead(200,{'Content-Type': 'text/html'});
-    var htmlResponse = fs.readFileSync(__dirname + '/index.html');
+    var htmlResponse = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+    var name = 'Raj';
+    htmlResponse = htmlResponse.replace('{Name}', name);
     res.end(htmlResponse);
 
 }).listen(12345,'127.0.0.1');
