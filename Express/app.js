8694896208b23ec7b84349eca59622847c4a40ee
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-03-14 19:06:50 -0400

    assets in express

--- Express/app.js
+++ Express/app.js
@@ -3,6 +3,8 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
     res.send('<html><head></head><body><h1>Hello Raj!</h1></body></html>');
 });
