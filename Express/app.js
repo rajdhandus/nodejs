f92ba24b7e5cff09381b5f34382fc9ee061f72e7
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-03-14 22:32:34 -0400

    Express and EJS modules

--- Express/app.js
+++ Express/app.js
@@ -5,12 +5,22 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine','ejs');
+
+app.use('/', function(req, res, next){
+    console.log('Request URL is :' + req.url);
+    next();
+});
+
 app.get('/', function(req, res){
-    res.send('<html><head><link href="assets/style.css type=text/css rel=stylesheet></head><body><h1>Hello Raj!</h1></body></html>');
+    res.render('index');
+    //res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello Raj!</h1></body></html>');
 });
 
 app.get('/person/:id', function(req, res){
-    res.send('<html><head></head><body><h1>Hello ' + req.params.id + '</h1></body></html>');
+    //res.send('<html><head></head><body><h1>Hello ' + req.params.id + '</h1></body></html>');
+
+    res.render('person', { ID : req.params.id });
 });
 
 app.get('/api', function(req, res){
